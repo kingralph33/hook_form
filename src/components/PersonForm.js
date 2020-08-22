f99b04d5136b41757e33a8e94fb18ab82000c02e
Ralph King JR <ralph@Ralphs-MacBook-Pro.local>
2020-08-22 16:28:37 -0400

    finished project using one component. Will update next to use two components and update state

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -1,48 +1,84 @@
-import React, { usetState } from "react";
+import React, { useState } from "react";
 
-import styles from "./PersonForm.module.scss";
+import "./PersonForm.scss";
 
 const PersonForm = (props) => {
-  // // variables for first name, last name, email, password
-  // const [fName, setFName] = useState("");
-  // const [lName, setLName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
+  // variables for first name, last name, email, password
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   return (
     <div>
       <div>
-        <form className={styles.form}>
-          <div className={styles.fields}>
-            <label className={styles.label}>First Name</label>
-            <input type="text" className={styles.input} />
+        <form className="form">
+          <div className="fields">
+            <label htmlFor="fName" className="label">
+              First Name
+            </label>
+            <input
+              type="text"
+              name="fName"
+              className="input"
+              onChange={(event) => setFName(event.target.value)}
+            />
           </div>
-          <div className={styles.fields}>
-            <label className={styles.label}>Last Name</label>
-            <input type="text" className={styles.input} />
+          <div className="fields">
+            <label htmlFor="lName" className="label">
+              Last Name
+            </label>
+            <input
+              type="text"
+              name="lName"
+              className="input"
+              onChange={(event) => setLName(event.target.value)}
+            />
           </div>
-          <div className={styles.fields}>
-            <label className={styles.label}>Email</label>
-            <input type="text" className={styles.input} />
+          <div className="fields">
+            <label htmlFor="email" className="label">
+              Email
+            </label>
+            <input
+              type="text"
+              name="email"
+              className="input"
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
-          <div className={styles.fields}>
-            <label className={styles.label}>Password</label>
-            <input type="password" className={styles.input} />
+          <div className="fields">
+            <label htmlFor="password" className="label">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              className="input"
+              onChange={(event) => setPassword(event.target.value)}
+            />
           </div>
-          <div className={styles.fields}>
-            <label className={styles.label}>Confirm Password</label>
-            <input type="password" className={styles.input} />
+          <div className="fields">
+            <label htmlFor="confirmPassword" className="label">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              className="input"
+              onChange={(event) => setConfirmPassword(event.target.value)}
+            />
           </div>
         </form>
       </div>
       <div>
-        <section className={styles.showFormData}>
-          <h1 className={styles.dataHeader}>Your Form Data</h1>
-          <p>First Name</p>
-          <p>Last Name</p>
-          <p>Email</p>
-          <p>Password</p>
-          <p>Confirm Password</p>
+        <section className="showFormData">
+          <h1 className="dataHeader">Your Form Data</h1>
+          <p>First Name: {fName}</p>
+          <p>Last Name: {lName}</p>
+          <p>Email: {email}</p>
+          <p>Password: password</p>
+          <p>Confirm Password: password</p>
         </section>
       </div>
     </div>
